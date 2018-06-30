8d1c670d31f10af5c2f19c7ee501d91cf1cc3ce4
Ahsan <ms02743@st.habib.edu.pk>
2018-07-01 04:06:28 +0500

    ap

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+var appsetting={
+    consumerkey: "7LgxFsYTRWAmbdDEIxUzPjPqK",
+    consumersecret: "hj2uMGAN5z5QTcC1bEYLegHRcYDqFkXdOOQ7iVPRzVseOzt7Jr",
+    bearertoken: ""
+};
+
+module.exports=appsetting;
